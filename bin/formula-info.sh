e1fe663e8eaf8aa53acc07ae207c399f7553c0e0
Nat! <nat@mulle-kybernetik.com>
2017-06-14 15:37:44 +0200

    Use templates to create .md files where necessary

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,4 +1,4 @@
-PROJECT="MulleConcurrent"   # your project name, requires camel-case
+PROJECT="mulle-concurrent"
 DESC="Lock- and Wait-free Hashtable (and an Array too) in C"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-allocator
 ${DEPENDENCY_TAP}mulle-aba
