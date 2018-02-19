90c908d3994a404c9fd5c8c3664a956dfca67ff4
Nat! <nat@mulle-kybernetik.com>
2018-02-19 23:49:51 +0100

    remove .bootstrap, remove bin, add mulle-project, remove templates

--- bin/formula-info.sh
+++
@@ -1,5 +0,0 @@
-PROJECT="mulle-concurrent"
-DESC="Lock- and Wait-free Hashtable (and an Array too) in C"
-DEPENDENCIES='${DEPENDENCY_TAP}mulle-allocator
-${DEPENDENCY_TAP}mulle-aba
-${DEPENDENCY_TAP}mulle-thread'
