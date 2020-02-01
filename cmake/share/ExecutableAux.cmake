da25ca711971467d34e52c78656b9b1e3003c89d
Nat! <nat@mulle-kybernetik.com>
2020-02-01 15:57:15 +0100

    upgraded mulle-sde improved travis-ci

--- cmake/share/ExecutableAux.cmake
+++ cmake/share/ExecutableAux.cmake
@@ -1,3 +1,7 @@
+### If you want to edit this, copy it from cmake/share to cmake. It will be
+### picked up in preference over the one in cmake/share. And it will not get
+### clobbered with the next upgrade.
+
 if( MULLE_TRACE_INCLUDE)
    message( STATUS "# Include \"${CMAKE_CURRENT_LIST_FILE}\"" )
 endif()
@@ -9,7 +13,6 @@ include( ExecutableAuxC OPTIONAL)
 
 CreateForceAllLoadList( ALL_LOAD_DEPENDENCY_LIBRARIES FORCE_ALL_LOAD_DEPENDENCY_LIBRARIES)
 CreateForceAllLoadList( STARTUP_ALL_LOAD_DEPENDENCY_LIBRARIES FORCE_STARTUP_ALL_LOAD_DEPENDENCY_LIBRARIES)
-
 
 if( NOT EXECUTABLE_LIBRARY_LIST)
    set( EXECUTABLE_LIBRARY_LIST
